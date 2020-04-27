0aea940bc1d316d93bf27fa19482953edc2f3c87
MezoneOrange <mezor07@mail.ru>
2020-04-27 21:22:47 +0500

    Modified login_page tests

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,7 @@ from .pages.login_page import LoginPage
 
 
 MAIN_LINK = "http://selenium1py.pythonanywhere.com/"
-LOGIN_LINK = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+# LOGIN_LINK = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
 
 @pytest.mark.main_page
@@ -24,19 +24,26 @@ def test_guest_should_see_login_link(browser):
 
 @pytest.mark.login_page
 def test_forms_should_be_in_login_in_url(browser):
-    login_page = LoginPage(browser, LOGIN_LINK)
+    page = MainPage(browser, MAIN_LINK)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_url()
 
 
 @pytest.mark.login_page
 def test_forms_should_be_in_login_form(browser):
-    login_page = LoginPage(browser, LOGIN_LINK)
-    login_page.open()
+    page = MainPage(browser, MAIN_LINK)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_form()
 
 
 @pytest.mark.login_page
 def test_forms_should_be_in_register_form(browser):
-    login_page = LoginPage(browser, LOGIN_LINK)
-    login_page.open()
+    page = MainPage(browser, MAIN_LINK)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_register_form()
