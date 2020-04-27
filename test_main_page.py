33e9326c6f9053b52c65d3fb4242eb5124c0bc23
MezoneOrange <mezor07@mail.ru>
2020-04-27 21:13:23 +0500

    Created pages/login_page.py and pytest.ini . Created LoginPage(BasePage) class in login_page.py. Created new locators in locators.py for LoginPage class. Created 3 new tests for login page.

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,42 @@
+import pytest
+
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
+MAIN_LINK = "http://selenium1py.pythonanywhere.com/"
+LOGIN_LINK = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+
+
+@pytest.mark.main_page
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, MAIN_LINK)
     page.open()
     page.go_to_login_page()
 
 
+@pytest.mark.main_page
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, MAIN_LINK)
     page.open()
     page.should_be_login_link()
+
+
+@pytest.mark.login_page
+def test_forms_should_be_in_login_in_url(browser):
+    login_page = LoginPage(browser, LOGIN_LINK)
+    login_page.should_be_login_url()
+
+
+@pytest.mark.login_page
+def test_forms_should_be_in_login_form(browser):
+    login_page = LoginPage(browser, LOGIN_LINK)
+    login_page.open()
+    login_page.should_be_login_form()
+
+
+@pytest.mark.login_page
+def test_forms_should_be_in_register_form(browser):
+    login_page = LoginPage(browser, LOGIN_LINK)
+    login_page.open()
+    login_page.should_be_register_form()
