d2a19a5cb1d27adcec4e687e3715998ebb8a8589
MezoneOrange <mezor07@mail.ru>
2020-04-29 14:39:06 +0500

    Created 2 tests for move from product page to basket page and check that basket is empty into test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import pytest
 
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 ITEM_LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -107,3 +108,21 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_form()
+
+
+@pytest.mark.empty_basket
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = ProductPage(browser, ITEM_LINK_3)
+    page.open()
+    page.go_to_basket_page()
+    basket = BasketPage(browser, browser.current_url)
+    basket.basket_is_empty()
+
+
+@pytest.mark.empty_basket
+def test_guest_can_see_basket_empty_message(browser):
+    page = ProductPage(browser, ITEM_LINK_3)
+    page.open()
+    page.go_to_basket_page()
+    basket = BasketPage(browser, browser.current_url)
+    basket.basket_is_empty_text()
