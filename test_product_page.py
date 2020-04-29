d69a0605c7af0aa49064ec681ff6a6b6e9333224
MezoneOrange <mezor07@mail.ru>
2020-04-29 11:53:54 +0500

    Added pytest parametrize to test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,30 +1,38 @@
 import pytest
 
 from .pages.product_page import ProductPage
-from .pages.locators import ProductPageLocators
 
 
-ITEM_LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# ITEM_LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
 
+@pytest.mark.parametrize('link',
+                         ['http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear',
+                          'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'])
 @pytest.mark.product_page
-def test_guest_can_add_product_to_basket(browser):
-    product_page = ProductPage(browser, ITEM_LINK)
+def test_guest_can_add_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
 
 
+@pytest.mark.parametrize('link',
+                         ['http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear',
+                          'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'])
 @pytest.mark.product_page
-def test_item_should_be_into_basket(browser):
-    product_page = ProductPage(browser, ITEM_LINK)
+def test_item_should_be_into_basket(browser, link):
+    product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
     product_page.item_should_be_into_basket()
 
 
+@pytest.mark.parametrize('link',
+                         ['http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear',
+                          'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'])
 @pytest.mark.product_page
-def test_basket_value_and_item_value_should_be_same(browser):
-    product_page = ProductPage(browser, ITEM_LINK)
+def test_basket_value_and_item_value_should_be_same(browser, link):
+    product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
     product_page.basket_value_and_item_value_should_be_same()
