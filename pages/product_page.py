0b28628cd5cce035d94f7de83472d064a266650c
MezoneOrange <mezor07@mail.ru>
2020-04-29 12:33:52 +0500

    Added 2 methods to base_page.py and one to product_page.py. Tried realize test by not presented element.

--- pages/product_page.py
+++ pages/product_page.py
@@ -17,3 +17,7 @@ class ProductPage(BasePage):
         item_cost = self.browser.find_element(*ProductPageLocators.ITEM_COST).text
         basket_cost = self.browser.find_element(*ProductPageLocators.TOTAL_BASKET).text
         assert item_cost == basket_cost, "item cost {} != basket cost {}".format(item_cost, basket_cost)
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.NAME_ITEM_IN_BASKET), \
+            "Success message is presented, but should not be"
